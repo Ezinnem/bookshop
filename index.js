25e4f4033fd8e8ac4f714dbb25bc3f53464e6545
martina <you@example.com>
2023-04-13 05:53:01 +0100

    chore; implemented the server file using express

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express();
+
+const bookController = require('./controllers/bookController');
+
+
+const mongoose = require('mongoose');
+
+//db config and options 
+let config = require('config');
+let options = { useNewUrlParser: true, useUnifiedTopology: true }
+
+//db connection      
+mongoose.connect(config.DBHost, options).then(() => {
+    console.log('Database connected..')
+})
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.post('/createBook', bookController.createBook);
+
+app.get('/getAllBooks', bookController.getAllBooks);
+
+app.get('/book/:id', bookController.getOneBook);
+
+app.patch('/updateBook/:id', bookController.updateOneBook);
+
+app.delete('/deleteBook/:id', bookController.deleteOneBook)
+
+const PORT = 1994
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}...`)
+})
